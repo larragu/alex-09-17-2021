b97f9a4072136494cac044ac3241524c603b629d
alarragu <alex.larragueta@gmail.com>
2021-10-12 09:15:16 -0700

    test: Notification component

---
+++ src/components/Notification/Notification.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+
+import Notification from './Notification';
+
+test('renders Notification for mobile', () => {
+  const reconnectHandlerMock = jest.fn();
+
+  const { container, getByText } = render(
+    <Notification 
+      reconnect={reconnectHandlerMock}
+    />
+  );
+
+  const buttonEl = getByText('Orderbook Disconnected: RECONNECT');
+  userEvent.click(buttonEl);
+ 
+  expect(container).toBeTruthy();
+  expect(reconnectHandlerMock.mock.calls.length).toEqual(1);
+}); 
